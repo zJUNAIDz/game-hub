f66d734ad3f46972b60a8a4ae4e77811ce280860
Junaid Shaikh <zjunaidshaikhz@gmail.com>
2023-04-29 11:11:47 +0530

    Build a Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
-function App() {
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
+const App = () => {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid templateAreas={{
+        base:`"nav" "main"`,
+        lg:`"nav nav" "aside main"`,//1024px
+
+      }}>
+        <GridItem style={{ backgroundColor: "green" }} area={"nav"}>
+          Nav
+        </GridItem>
+
+        <Show above="lg">
+          <GridItem style={{ backgroundColor: "red" }} area={"aside"}>
+            aside
+          </GridItem>
+        </Show>
+        <GridItem style={{ backgroundColor: "purple" }} area={"main"}>
+          main
+        </GridItem>
+      </Grid>
     </>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
