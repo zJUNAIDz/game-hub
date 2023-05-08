c6f51119ebb248c22729181683e3d88e8e7d77aa
Junaid Shaikh <zjunaidshaikhz@gmail.com>
2023-05-08 21:52:11 +0530

    Handle displaying games with no image by adding no image placehloder svg

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,10 @@
-const getCroppedImageURL = (URL:string) => {
-  const target = 'media/'
+import noImageFound from "../assets/no_image_placeholder.svg";
+
+const getCroppedImageURL = (URL: string) => {
+  if (!URL) return noImageFound;
+  const target = "media/";
   const index = URL.indexOf(target) + target.length;
-  return URL.slice(0,index) + 'crop/600/400/' + URL.slice(index)
+  return URL.slice(0, index) + "crop/600/400/" + URL.slice(index);
 };
 
 export default getCroppedImageURL;
