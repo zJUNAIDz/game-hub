1b98b2fed7fdf189e77dad705c62e0def10204cb
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-08-19 16:40:29 +0530

    feat: make sticky navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, useColorMode } from "@chakra-ui/react";
 import logo from "../assets/igdb-logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { colorMode } = useColorMode();
+
   return (
-    <HStack padding={2}>
+    <HStack padding={2} justifyContent="space-between" bg={colorMode === "dark" ? "gray.900" : "white"} boxShadow="md">
       <Link to="/">
         <Image objectFit="cover" borderRadius={5} boxSize="50px" src={logo} />
       </Link>
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -5,9 +5,11 @@ import { Box } from "@chakra-ui/react";
 const Layout = () => {
   return (
     <>
+      <Box position="sticky" top="0" zIndex="1000">
       <Navbar />
+      </Box>
       <Box padding={5}>
-        <Outlet />
+      <Outlet />
       </Box>
     </>
   );
